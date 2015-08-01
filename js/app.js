3a82a3cc522ae80e9c5509b98d9ad23491fd6357
Asim Hussain <jawache@gmail.com>
2015-08-01 17:57:53 +0100

    Added assets and initial controller

--- js/app.js
+++ js/app.js
@@ -1,19 +1,43 @@
-// Ionic Starter App
+var app = angular.module('8ball', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+app.run(function ($ionicPlatform) {
+	$ionicPlatform.ready(function () {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
+});
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+
+app.controller('PredictionController', function ($scope) {
+
+	var predictionList = [
+		"Signs point to yes",
+		"Yes",
+		"Reply hazy, try again",
+		"Without a doubt",
+		"My sources say no",
+		"As I see it, yes",
+		"You may rely on it",
+		"Concentrate and ask again",
+		"Outlook not so good",
+		"It is decidedly so",
+		"Better not tell you now",
+		"Very doubtful",
+		"Yes - definitely",
+		"It is certain",
+		"Cannot predict now",
+		"Most likely",
+		"Ask again later",
+		"My reply is no",
+		"Outlook good",
+		"Don't count on it"
+	];
+
+	$scope.prediction = "Tap 8ball for an answer";
+});
